77462b19fd058b42361bd6b30554aa195f7672c6
Diego Andres Cabrera Rojas <diegoandresrojas2000@gmail.com>
2020-11-25 18:25:05 -0500

    Home & checkout

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/components/Footer.css';
+
+const Footer = () => {
+  return (
+    <div className="Footer">
+      <p className="Footer-title">Platzi Conf Merch</p>
+      <div className="Footer-copy">Todos los Izquierdos Reservados</div>
+    </div>
+  );
+};
+export default Footer;
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,62 @@
-const path = require("path");
-const HtmlWebpackPlugn = require("html-webpack-plugin");
-
+const path = require('path');
+const HtmlWebpackPlugn = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+var mode = process.env.NODE_ENV || 'development';
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
   },
+  devtool: mode === 'development' ? 'inline-source-map' : false,
+  mode: mode,
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
       },
       {
         test: /\.html$/,
-        use: {
-          loader: "html-loader",
-        },
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          'css-loader',
+        ],
       },
     ],
   },
-
   plugins: [
     new HtmlWebpackPlugn({
-      template: "./public/index.html",
-      filename: "./index.html",
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'assets/[name].css',
     }),
   ],
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 3005,
+    historyApiFallback: true,
   },
 };
